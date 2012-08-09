2d51e960c79e5a4ded748e229b75253b18294a11
Tomer Filiba <tomerf@il.ibm.com>
2012-08-09 11:26:18 +0300

    fix test

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 rm -rf build/ dist/ *.egg-info/
-python setup.py register sdist --formats=gztar,zip upload
-python setup.py bdist_wininst --plat-name=win32 upload
+python setup.py register sdist --formats=gztar,zip bdist_wininst --plat-name=win32 upload
--- tests/test_reedsolo.py
+++ tests/test_reedsolo.py
@@ -10,32 +10,32 @@ except NameError:
 class TestReedSolomon(unittest.TestCase):
     def test_simple(self):
         rs = RSCodec(10)
-        msg = b"hello world " * 10
+        msg = bytearray("hello world " * 10, "utf8")
         enc = rs.encode(msg)
         dec = rs.decode(enc)
-        self.assertEquals(dec, bytearray(msg))
+        self.assertEqual(dec, msg)
     
     def test_correction(self):
         rs = RSCodec(10)
-        msg = b"hello world " * 10
+        msg = bytearray("hello world " * 10, "utf8")
         enc = rs.encode(msg)
-        self.assertEquals(rs.decode(enc), bytearray(msg))
+        self.assertEqual(rs.decode(enc), msg)
         for i in [27, -3, -9, 7, 0]:
             enc[i] = 99
-            self.assertEquals(rs.decode(enc), bytearray(msg))
+            self.assertEqual(rs.decode(enc), msg)
         enc[82] = 99
         self.assertRaises(ReedSolomonError, rs.decode, enc)
     
     def test_long(self):
         rs = RSCodec(10)
-        msg = b"a" * 10000
+        msg = bytearray("a" * 10000, "utf8")
         enc = rs.encode(msg)
         dec = rs.decode(enc)
-        self.assertEquals(dec, bytearray(msg))
+        self.assertEqual(dec, msg)
         enc[177] = 99
         enc[2212] = 88
         dec2 = rs.decode(enc)
-        self.assertEquals(dec2, bytearray(msg))
+        self.assertEqual(dec2, msg)
 
 
 if __name__ == "__main__":

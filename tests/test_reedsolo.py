c618cfaa54a232bf04d46dc9339c0232ebbc315d
rotorgit <rotorgit@users.noreply.github.com>
2015-02-07 15:23:19 -0500

    add unit tests for new features
    - added tests for new features, optional user-supplied fcr (first consecutive root), and primitive polynomial
    - original tests unmodified (will default to fcr = 0 and primitive poly = 0x11d, as before)

--- tests/test_reedsolo.py
+++ tests/test_reedsolo.py
@@ -1,5 +1,6 @@
 import unittest
 from reedsolo import RSCodec, ReedSolomonError
+from random import randint, sample
 
 try:
     bytearray
@@ -36,7 +37,61 @@ class TestReedSolomon(unittest.TestCase):
         enc[2212] = 88
         dec2 = rs.decode(enc)
         self.assertEqual(dec2, msg)
+        
+    def test_prim_fcr_basic(self):
+        nn = 30
+        kk = 18
+        tt = nn - kk
+        rs = RSCodec(tt, fcr=120, prim=0x187)
+        hexencmsg = '00faa123555555c000000354064432c02800fe97c434e1ff5365' \
+            'cf8fafe4'
+        encmsg = bytearray(hexencmsg.decode("hex"))
+        decmsg = encmsg[:kk]
+        tem = rs.encode(decmsg)
+        self.assertEqual(encmsg, tem, msg="encoded does not match expected")
+        tdm = rs.decode(tem)
+        self.assertEqual(tdm, decmsg, msg="decoded does not match original")
+        tem1 = bytearray(tem) # clone a copy
+        # encoding and decoding intact message seem OK, so test errors
+        numerrs = tt >> 1 # inject tt/2 errors (expected to recover fully)
+        for i in sample(xrange(nn), numerrs): # inject errors in random places
+            tem1[i] ^= 0xff # flip all 8 bits
+        tdm = rs.decode(tem1)
+        self.assertEqual(tdm, decmsg,
+            msg="decoded with errors does not match original")
+        tem1 = bytearray(tem) # clone another copy
+        numerrs += 1 # inject tt/2 + 1 errors (expected to fail and detect it)
+        for i in sample(xrange(nn), numerrs): # inject errors in random places
+            tem1[i] ^= 0xff # flip all 8 bits
+        # if this fails, it means excessive errors not detected
+        self.assertRaises(ReedSolomonError, rs.decode, tem1)
 
+    def test_prim_fcr_long(self):
+        nn = 48
+        kk = 34
+        tt = nn - kk
+        rs = RSCodec(tt, fcr=120, prim=0x187)
+        hexencmsg = '08faa123555555c000000354064432c0280e1b4d090cfc04887400' \
+            '000003500000000e1985ff9c6b33066ca9f43d12e8'
+        encmsg = bytearray(hexencmsg.decode("hex"))
+        decmsg = encmsg[:kk]
+        tem = rs.encode(decmsg)
+        self.assertEqual(encmsg, tem, msg="encoded does not match expected")
+        tdm = rs.decode(tem)
+        self.assertEqual(tdm, decmsg, 
+            msg="decoded does not match original")
+        tem1 = bytearray(tem)
+        numerrs = tt >> 1
+        for i in sample(xrange(nn), numerrs):
+            tem1[i] ^= 0xff
+        tdm = rs.decode(tem1)
+        self.assertEqual(tdm, decmsg,
+            msg="decoded with errors does not match original")
+        tem1 = bytearray(tem)
+        numerrs += 1
+        for i in sample(xrange(nn), numerrs):
+            tem1[i] ^= 0xff
+        self.assertRaises(ReedSolomonError, rs.decode, tem1)
 
 if __name__ == "__main__":
     unittest.main()

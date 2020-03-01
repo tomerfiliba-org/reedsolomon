f8ef23d7ec0607d841ad26c8b3c21561a5e4922b
Stephen L <lrq3000@gmail.com>
2020-03-01 20:40:22 +0100

    drop Python 3.8 support for the moment, not yet compatible with Cython + fix coverage
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(name = "reedsolo",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        #"Programming Language :: Python :: 3.8",  # not yet compatible with Cython (as of March 2020)
         "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Cython",
         "Topic :: Communications",

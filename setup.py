142fc1d98f6ba469d82829c3ff869de97b1592dc
Stephen L <lrq3000@gmail.com>
2022-12-07 21:30:48 +0100

    Bump v1.5.5, merge #38. Fix #36, #49.
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ except ImportError:
         extensions = None
 
 setup(name = "reedsolo",
-    version = "1.5.4",
+    version = "1.5.5",
     description = "Pure-Python Reed Solomon encoder/decoder",
     author = "Tomer Filiba",
     author_email = "tomerfiliba@gmail.com",

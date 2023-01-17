46cb9e90d5c3607d78736552511f765741a18541
Stephen L <lrq3000@gmail.com>
2023-01-17 05:54:00 +0100

    update documentation + bump v1.7.0 (major release because we ditch cythonization by default)
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ else:
     cmdclass = {}
 
 setup(name = "reedsolo",
-    version = "1.6.1",
+    version = "1.7.0",
     description = "Pure-Python Reed Solomon encoder/decoder",
     author = "Tomer Filiba",
     author_email = "tomerfiliba@gmail.com",

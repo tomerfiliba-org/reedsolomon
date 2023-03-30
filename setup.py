9d3be98064f68ba3c33d04bbb8b5e6e7504ad416
Stephen L <lrq3000@gmail.com>
2023-03-31 00:28:23 +0200

    bump v2.0.0 (because of breaking changes, especially in creedsolo.pyx)
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
     cmdclass = {}
 
 setup(name = "reedsolo",
-    version = "1.7.28",
+    version = "2.0.0",
     description = "Pure-Python Reed Solomon encoder/decoder",
     author = "Tomer Filiba",
     author_email = "tomerfiliba@gmail.com",

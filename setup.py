b85ac5812c5193762eb7b2818bc82c746cb3ead5
Stephen L <lrq3000@gmail.com>
2023-03-30 22:16:24 +0200

    bump v1.7.26
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
     cmdclass = {}
 
 setup(name = "reedsolo",
-    version = "1.7.25",
+    version = "1.7.26",
     description = "Pure-Python Reed Solomon encoder/decoder",
     author = "Tomer Filiba",
     author_email = "tomerfiliba@gmail.com",

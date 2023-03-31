f2f6b29b0d63ab66f41a28ce95a1a25743bc1b70
Stephen L <lrq3000@gmail.com>
2023-03-31 11:26:48 +0200

    bump v2.0.6
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if '--cythonize' in sys.argv:
             #extensions = [ Extension('creedsolo', ['creedsolo.c']) ]
         #else:
         # Else run in pure python mode (no compilation)
-        print("Cython is not installed or is explicitly skipped using --nocython, no creedsolo module will be built")
+        print("Cython is not installed despite specifying --cythonize, no creedsolo module will be built")
         extensions = None
         cmdclass = {}
 else:
@@ -50,7 +50,7 @@ else:
     cmdclass = {}
 
 setup(name = "reedsolo",
-    version = "2.0.5",
+    version = "2.0.6",
     description = "Pure-Python Reed Solomon encoder/decoder",
     author = "Tomer Filiba",
     author_email = "tomerfiliba@gmail.com",
